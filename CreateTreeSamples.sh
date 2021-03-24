55add770445886454f3907863e1af1342e8c5a03
= <=>
2021-03-24 09:21:07 +0100

    path adapted

--- CreateTreeSamples.sh
+++ CreateTreeSamples.sh
@@ -4,13 +4,15 @@ cd code
 julia createData.jl
 # run MrBayes for eligible files
 # CAUTION: TIME INTENSIVE
-cd ../data/asjpNex/
+cd mrbayes
 for f in *mb.nex; do
+    bn=`basename $f .mb.nex`;
     mb $f;
+    mb $bn".mb1.nex";
 done
 # run RevBayes for for eligible files
 # CAUTION: TIME INTENSIVE
-cd ../../code/revbayes
+cd ../revbayes
 for f in *Rev; do
     rb $f;
 done
